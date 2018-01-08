b9396acbd812a74bd1b04f65c91f99ec5f3d484c
den-isk1995 <den-isk1995@mail.ru>
2018-01-08 20:57:26 +0700

    Created new function for searching of TOP-5 available paths between point in some graph Fixed test 'graph1'

--- app/api/set_path.py
+++ app/api/set_path.py
@@ -1,6 +1,6 @@
 import pprint
 from operator import itemgetter
-
+from copy import deepcopy
 
 def generate_roads(graph, time=None, max_time=None):
     dist = {}
@@ -60,18 +60,68 @@ def generate_roads(graph, time=None, max_time=None):
     res = sorted(res, key=itemgetter('point'))
     return res
 
+top_paths = []
+top_count = 5
+# It's recursion function for top_count paths finding from begin_point to end_point
+# and value of such paths less then max_time
+def longest_paths(begin_point, end_point, current_point, graph, time, max_time, visited=None, current_path=None):
+    global top_paths
+    global top_count
+
+    if len(top_paths) == top_count:
+        return 1
+
+    if begin_point == current_point:
+        current_path = ([0], time[0])
+        visited = [0] * (end_point + 1)
+        print(current_path, visited)
+
+    if end_point == current_point:
+        if current_path[1] <= max_time \
+        and len(top_paths) < top_count \
+        and current_path not in top_paths:
+            top_paths.append({'path':current_path[0], 'point':current_path[1]})
+
+        if len(top_paths) == top_count:
+            return 1
+        else:
+            return 0
+
+    visited[current_point] = 1
+    for i in range(begin_point, end_point + 1):
+        if graph[current_point][i] and visited[i] == 0:
+            tmp = deepcopy(current_path)
+            tmp[0].append(i)
+            tmp = (tmp[0], tmp[1] + graph[current_point][i] + time[i])
+
+            if longest_paths(begin_point, end_point, i, graph, time, max_time, visited, tmp) == 1:
+                return 1
+
+    visited[current_point] = 0
+    return 0
+
+def get_top_paths(graph, time, max_time):
+    global top_paths
+    global top_count
+    top_paths = []
+    longest_paths(0, len(graph) - 1, 0, graph, time, max_time)
+    return sorted(top_paths, key=itemgetter('point'))
+
 graph = {0: {1: 10, 2: 70, 5: 20},
          1: {2: 20, 3: 30, 4: 40},
          2: {3: 15, 4: 10},
          3: {4: 30, 5: 60},
          4: {5: 20},
          5: {}}
-graph1 = {0: {0: 1, 1: 189, 2: 88, 3: 100, 4: 97},
-         1: {0: 186, 1: 1, 2: 115, 3: 112, 4: 107},
-         2: {0: 87, 1: 117, 2: 1, 3: 13, 4: 10},
-         3: {0: 98, 1: 114, 2: 13, 3: 1, 4: 10},
-         4: {0: 96, 1: 110, 2: 9, 3: 10, 4: 1}}
+graph1 = {0: {0: 0,   1: 186, 2: 87,  3: 98,  4: 96},
+          1: {0: 186, 1: 0,   2: 117, 3: 114, 4: 110},
+          2: {0: 87,  1: 117, 2: 0,   3: 13,  4: 9},
+          3: {0: 98,  1: 114, 2: 13,  3: 0,   4: 10},
+          4: {0: 96,  1: 110, 2: 9,   3: 10,  4: 0}}
 #time = [14, 15, 16, 17, 18]
 #max_time = 240
 #result = generate_roads(graph1, time, max_time)
 #pprint.pprint(result)
+
+#res = get_top_paths(graph1, time, max_time)
+#pprint.pprint(res)
