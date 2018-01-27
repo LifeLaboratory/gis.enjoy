94a09aa3779ad97ad1d07a7a338dc54eb2dd3a6f
raldenprog <raldenprog@mail.ru>
2018-01-27 19:00:09 +0700

    нормолизатор с bug

--- app/api/config.py
+++ app/api/config.py
@@ -1,1 +1,9 @@
 HEADER = {'Access-Control-Allow-Origin': '*'}
+DATABASE = {
+    "dbname": "life_game_service",
+    "user": "life_dev",
+    "host": "90.189.132.25",
+    "password": "PINLOX!@#"
+}
+
+INDEXES = ["Парк", "Памятник", "Музей", "Театр", "Галерея"]
