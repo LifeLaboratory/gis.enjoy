341e7bfebc3ef2b3c6aceace8c517b942dcdc7eb
Anton Chusovitin <antonion2011@yandex.ru>
2018-02-18 23:57:16 +0700

    * перенёс в класс path файл set_path.py

--- app/api/path.py
+++ app/api/path.py
@@ -1,4 +1,6 @@
 import math
+from operator import itemgetter
+from copy import deepcopy
 from api.helpers.sql import Sql
 from api.google.helpers.google import Google
 __author__ = 'ar.chusovitin'
@@ -148,11 +150,8 @@ class Path:
         self.dict_graph[0][N] = t
 
     def filtered_graph(self):
-
-
-        # print('graph  =  ', graph)
         for i in range(len(self.dict_graph)):
-            if i > 57 and i < 81:
+            if 57 < i < 81:
                 continue
             self.new_graph[i] = []
             for j in range(len(self.dict_graph[i])):
@@ -161,10 +160,10 @@ class Path:
                 elif j == len(self.dict_graph) - 1:
                     self.new_graph[i].append((j, self.dict_graph[i][j], 0, 0))
                 elif j > 0:
-                    if j > 57 and j < 81:
+                    if 57 < i < 81:
                         continue
-                    #print(len(self.dict_coords), j)
-                    #tyobj = INDEXES.get(self.dict_coords[j]['Type'], 0)
+                    # print(len(self.dict_coords), j)
+                    # tyobj = INDEXES.get(self.dict_coords[j]['Type'], 0)
                     # print(tyobj)
                     try:
                         # new_graph[i].append((j, self.dict_graph[i][j], tyobj, self.dict_coords[j]['Rating']))
@@ -173,7 +172,7 @@ class Path:
                         pass
 
     def normalize_graph_coefficient(self):
-        #coefficiet_graph = normalize_point_data(new_graph, priority)
+        # coefficiet_graph = normalize_point_data(new_graph, priority)
 
         # print("!", coefficiet_graph)
         def new_element(l, element):
@@ -181,7 +180,7 @@ class Path:
             new_l.append(element)
             return tuple(new_l)
 
-        #for i in range(len(coefficiet_graph)):
+        # for i in range(len(coefficiet_graph)):
         #    for j in range(len(coefficiet_graph[i])):
         #        self.new_graph[i][j] = new_element(self.new_graph[i][j], coefficiet_graph[i][j][1])
         #    self.new_graph[i] = sorted(self.new_graph[i], key=lambda x: (x[1], x[4]))
@@ -212,3 +211,98 @@ class Path:
             answer['route'][ch]['type'].append('Touch')
             ch += 1
         return answer
+
+    def generate_roads(self, graph, time=None, max_time=None):
+        dist = {}
+        pred = {}
+        start = 0
+        end = len(graph) - 1
+        for u in graph:
+            dist[u] = {}
+            pred[u] = {}
+            for v in graph:
+                dist[u][v] = None
+                pred[u][v] = -1
+        for u in graph:
+            for neighbor in graph[u]:
+                dist[u][neighbor] = graph[u][neighbor]
+                pred[u][neighbor] = u
+                dist[neighbor][u] = graph[u][neighbor]
+            dist[u][u] = time[u]
+        pos_start = 0
+        pos_end = 1
+        result = {(0,): 0}
+        temp = [(0,)]
+        res = list()
+        for u in range(len(graph)):
+            for v in range(len(graph)):
+                for i in range(pos_start, pos_end):
+                    index = list(temp[i])
+                    pos = index[len(index) - 1]
+                    if v not in index and dist[pos][v]:
+                        index.append(v)
+                        old_index = tuple(index)
+                        temp.append(old_index)
+                        result[old_index] = result[temp[i]] + dist[pos][v] + dist[u][u]
+                        if old_index[0] == start \
+                                and old_index[len(old_index) - 1] == end \
+                                and result[old_index] <= max_time:
+                            res.append({'path': old_index, 'point': result[old_index]})
+            pos_start = pos_end
+            pos_end = len(temp)
+        res = sorted(res, key=itemgetter('point'))
+        return res
+
+    top_paths = []
+    top_count = 5
+
+    # It's recursion function for top_count paths finding from begin_point to end_point
+    # and value of such paths less then max_time
+    def longest_paths(
+            self, begin_point, end_point, current_point,
+            graph, time, max_time, visited=None, current_path=None
+    ):
+        global top_paths
+        global top_count
+        if len(top_paths) == top_count:
+            return 1
+
+        if begin_point == current_point:
+            current_path = ([0], time[0])
+            visited = [0] * (end_point + 1)
+
+        if end_point == current_point:
+            if current_path[1] <= max_time \
+                    and len(top_paths) < top_count \
+                    and current_path not in top_paths:
+                top_paths.append({'path': current_path[0], 'point': current_path[1]})
+
+            if len(top_paths) == top_count:
+                return 1
+            else:
+                return 0
+
+        visited[current_point] = 1
+        for i in range(begin_point, end_point + 1):
+            if graph[current_point][i][1] and visited[graph[current_point][i][0]] == 0:
+                tmp = deepcopy(current_path)
+                tmp[0].append(graph[current_point][i][0])
+                tmp = (tmp[0], tmp[1] + graph[current_point][i][1] + time[graph[current_point][i][0]])
+                if max_time < tmp[1]:
+                    return 0
+                if tmp[1] <= max_time:
+                    if self.longest_paths(
+                            begin_point, end_point, graph[current_point][i][0],
+                            graph, time, max_time,
+                            visited, tmp) == 1:
+                        return 1
+
+        visited[current_point] = 0
+        return 0
+
+    def get_top_paths(self, graph, time, max_time):
+        global top_paths
+        global top_count
+        top_paths = []
+        self.longest_paths(0, len(graph) - 1, 0, graph, time, max_time)
+        return sorted(top_paths, key=itemgetter('point'))
