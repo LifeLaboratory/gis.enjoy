8ce5a5b9f2065e0bdbeee895d34ee0bc4335cb13
raldenprog <raldenprog@mail.ru>
2018-04-14 01:33:48 +0700

    fix filter

--- app/api/filter.py
+++ app/api/filter.py
@@ -9,15 +9,11 @@ class Filter:
         self.__list_type = []
 
     def get_filter(self):
-        get_sql = """
-            with
-            filter as (
-    SELECT DISTINCT id, type FROM geo
-    )
-    SELECT type FROM filter
+        get_sql = """select distinct type FROM geo
         """
         dict_type = Sql.exec(get_sql)
         result = []
         for d in dict_type:
             result.append(d["type"])
         return {"data": result}
+print(Filter().get_filter())
