4dd536a1d5912ba661f0667206f8d08381466f14
den-isk1995 <den-isk1995@mail.ru>
2018-04-14 00:48:31 +0700

    * Добавил авторизацию, регистрацию, выход.

---
+++ app/route/Authentication.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+from flask_restful import Resource, reqparse
+
+import api.auth.auth as auth
+from api.helpers.json import converter
+
+class Authentication(Resource):
+    def __init__(self):
+        self.__parser = reqparse.RequestParser()
+        self.__parser.add_argument('data')
+        self.__parser.add_argument('param')
+        self.__args = self.__parser.parse_args()
+        self.data = None
+
+    def parse_data(self):
+        self.data = self.__args.get('data', None)
+        self.param = self.__args.get('param', None)
+        print("param:", self.param)
+        self.data = converter(self.data)
+        print("data: ", self.data)
+        return
+
+    def switch(self):
+        if self.param == "get_user_name" and self.data is not None:
+            self.data["id_user"] = auth.session_verification(self.data["UUID"])
+            print(self.data["id_user"])
+            answer = converter(converter(auth.get_user_name(self.data["id_user"])))
+            return answer
+        else:
+            answer = auth.login_verification(self.data)
+            return answer
+
+    def get(self):
+        try:
+            print("Auth")
+            self.parse_data()
+
+            answer = self.switch()
+            print("answer: ", answer)
+            return answer, 200, {'Access-Control-Allow-Origin': '*'}
+
+        except:
+            return "Error", 200, {'Access-Control-Allow-Origin': '*'}
