18bc37d644e02ad55810669dcc51a23e48e04ff3
Anton Chusovitin <antonion2011@yandex.ru>
2018-04-13 18:15:27 +0700

    * Обновил конфигурацию

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 HEADER = {'Access-Control-Allow-Origin': '*'}
 DATABASE = {
-    "dbname": "life_game_service",
-    "user": "life_dev",
+    "dbname": "GIS",
+    "user": "gis_root",
     "host": "90.189.132.25",  # ip - для разработки
     # "host": "localhost",        # ip - для сервера
     "password": "PINLOX!@#"
