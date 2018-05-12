ee70ec78feed15d8765f7396c5321b06b45b3235
raldenprog <raldenprog@mail.ru>
2018-05-12 16:16:37 +0700

    +2 keys

--- app/api/google/helpers/key.py
+++ app/api/google/helpers/key.py
@@ -2,4 +2,7 @@ __author__ = 'ar.chusovitin'
 key = [
        "AIzaSyDN5wMmJY1m183ZFB8zj-Xomja3H3xB79M",
        "AIzaSyCjpScdLpGcn7tR_ZD4qS3-PnvfKaZMUic",
+       "AIzaSyAKJrXROnRp7r1oINBc1YkHfrCbNJ4uL2c",
+       "AIzaSyBzRSDemtvt_Ixo6ucQvdDutsEHcWBLk0k",
+       "AIzaSyC7xHAT0j6xCi6IEd9rMomYA_hab6TtFto"
 ]
